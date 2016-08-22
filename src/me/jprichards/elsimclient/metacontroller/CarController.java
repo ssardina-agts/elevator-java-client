ecd1d3bcd34401d8013f2d052525485fae561057
Josh Richards <justprogress108@gmail.com>
2016-08-23 03:01:50 +1000

    test commit. please ignore

--- src/me/jprichards/elsimclient/metacontroller/CarController.java
+++ src/me/jprichards/elsimclient/metacontroller/CarController.java
@@ -1,7 +1,10 @@
 package me.jprichards.elsimclient.metacontroller;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import me.jprichards.elsimclient.Direction;
 import me.jprichards.elsimclient.model.Car;
@@ -10,18 +13,42 @@ import me.jprichards.elsimclient.model.Floor;
 public class CarController
 {
 	private Car car;
-	private List<Floor> destinationQueue = new ArrayList<>();
-	private List<Floor> destinationQueueOtherDirection = new ArrayList<>();
+	private SortedSet<Floor> destinationQueue;
+	private SortedSet<Floor> destinationQueueOtherDirection;
+	Comparator<Floor> normal;
+	Comparator<Floor> other;
 	private Direction currentDirection = Direction.UP;
 	
 	public CarController(Car car)
 	{
 		this.car = car;
-	}
-
-
-	public FloorComparator implements Comparator<Floor>
-	{
+		normal = (Floor arg0, Floor arg1) ->
+		{
+			int diff = (int) (arg0.getHeight() - arg1.getHeight());
+			if (currentDirection == Direction.DOWN)
+			{
+				diff = 0 - diff;
+			}
+			
+			return diff;
+		};
 		
+		other = (Floor arg0, Floor arg1) ->
+			0 - normal.compare(arg0, arg1);
+		
+		destinationQueue = new TreeSet<>(normal);
+		destinationQueueOtherDirection = new TreeSet<>(other);
+	}
+	
+	public void addDestination(Floor f)
+	{
+		if (destinationQueue.contains(f) || destinationQueueOtherDirection.contains(f))
+		{
+			return;
+		}
+		
+		if (currentDirection == Direction.UP)
+		{
+		}
 	}
 }
